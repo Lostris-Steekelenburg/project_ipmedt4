466ce186e8c25fd2462a88e701f6dbce376cc7b9
Lostris-Steekelenburg <s1121524@student.hsleiden.nl>
2022-06-08 21:09:59 +0200

    Updates

---
+++ precache-manifest.63cb644c04da5c6585d721a30afb858f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "064d5b73fb59e21c4542",
+    "url": "/static/css/main.fb1724b9.chunk.css"
+  },
+  {
+    "revision": "064d5b73fb59e21c4542",
+    "url": "/static/js/main.064d5b73.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "2ded161d425ce65a7129",
+    "url": "/static/js/2.2ded161d.chunk.js"
+  },
+  {
+    "revision": "8ba3075663cac21e8fc5",
+    "url": "/static/js/runtime~main.8ba30756.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "1307555740c761bc6f07a07ae3354803",
+    "url": "/index.html"
+  }
+];
